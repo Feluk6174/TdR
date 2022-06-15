c9b01d710d806fae0b6892793668af72943b5068
Feluk6174 <feluk6174@proton.me>
2022-06-15 13:23:17 +0200

    Fixed databases.py to work with mysql

--- backend_test_4/database.py
+++ backend_test_4/database.py
@@ -16,16 +16,16 @@ def create(connection):
     cursor.execute("DROP TABLE IF EXISTS posts;")
     cursor.execute("DROP TABLE IF EXISTS users;")
 
-    cursor.execute("CREATE TABLE users(id INTEGER NOT NULL PRIMARY KEY, user_name VARCHAR(16) NOT NULL UNIQUE, public_key INTEGER NOT NULL UNIQUE, info VARCHAR(255);")
-    cursor.execute("CREATE TABLE posts(id INTEGER NOT NULL PRIMARY KEY, user_id INTEGER NOT NULL, post VARCHAR(255) NOT NULL, FOREIGN KEY (user_id) REFERENCES users (id));")
-    cursor.execute("CREATE TABLE coments(id INTEGER NOT NULL PRIMARY KEY, user_id INTEGER NOT NULL, post_id INTEGER NOT NULL, comment VARCHAR(255) NOT NULL, FOREIGN KEY (user_id) REFERENCES users (id), FOREIGN KEY (post_id) REFERENCES posts (id));")
+    cursor.execute("CREATE TABLE users(id INT NOT NULL PRIMARY KEY, user_name VARCHAR(16) NOT NULL UNIQUE, public_key INT NOT NULL UNIQUE, info VARCHAR(255));")
+    cursor.execute("CREATE TABLE posts(id INT NOT NULL PRIMARY KEY, user_id INT NOT NULL, post VARCHAR(255) NOT NULL, FOREIGN KEY (user_id) REFERENCES users (id));")
+    cursor.execute("CREATE TABLE comments(id INT NOT NULL PRIMARY KEY, user_id INT NOT NULL, post_id INT NOT NULL, comment VARCHAR(255) NOT NULL, FOREIGN KEY (user_id) REFERENCES users (id), FOREIGN KEY (post_id) REFERENCES posts (id));")
 
    
     cursor.execute("DROP TABLE IF EXISTS ips;")
     cursor.execute("DROP TABLE IF EXISTS conected;")
 
-    cursor.execute("CREATE TABLE ips(ip VARCHAR(21) NOT NULL PRIMARY KEY, time_connected INTEGER NOT NULL);")
-    cursor.execute("CREATE TABLE conected(ip VARCHAR(21) NOT NULL PRIMARY KEY, time_connected INTEGER NOT NULL);")
+    cursor.execute("CREATE TABLE ips(ip VARCHAR(21) NOT NULL PRIMARY KEY, time_connected INT NOT NULL);")
+    cursor.execute("CREATE TABLE connected_ips(ip VARCHAR(21) NOT NULL PRIMARY KEY, time_connected INT NOT NULL);")
 
     connection.commit()
 
