9934f8bbadb0677b4f3b2d99761ea39b5b82e182
Feluk6174 <feluk6174@proton.me>
2022-07-05 18:49:14 +0200

    changed name from setup_test_2.py to create_db.py

--- backend_test_4/create_db.py
+++ backend_test_4/create_db.py
@@ -5,4 +5,4 @@ ports = [11111, 22222, 33333, 44444]
 db = database.connection()
 db.create()
 
-db.execute(f"INSERT INTO ips(ip, time_connected) VALUES({ip}, {time.time()});")
+#db.execute(f"INSERT INTO ips(ip, time_connected) VALUES({ip}, {time.time()});")
