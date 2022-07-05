d6a793522379aa62fb8fa4da55e056280701218a
Feluk6174 <feluk6174@proton.me>
2022-07-05 22:09:47 +0200

    fixed small bug to test 4

--- backend_test_4/server_test_2.py
+++ backend_test_4/server_test_2.py
@@ -64,6 +64,7 @@ def connect_to_new_node():
     global connections
     while True:
         ip = db.querry("SELECT ip FROM ips ORDER BY RAND() LIMIT 1;")
+        print(ip)
         host, port = ip[0].split(":")
     
         connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
