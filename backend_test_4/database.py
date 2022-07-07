cc8f4aadc3751c6637242de84e7ebb3cb4c4caa8
Feluk6174 <feluk6174@proton.me>
2022-07-07 21:02:16 +0200

    changed database.py

--- backend_test_4/database.py
+++ backend_test_4/database.py
@@ -44,25 +44,25 @@ def execute(sql:str):
 
 
 def create():
-    with thread_lock:
-        cursor = connection.cursor()
+    connection = connect()
+    cursor = connection.cursor()
 
-        cursor.execute("DROP TABLE IF EXISTS comments;")
-        cursor.execute("DROP TABLE IF EXISTS posts;")
-        cursor.execute("DROP TABLE IF EXISTS users;")
+    cursor.execute("DROP TABLE IF EXISTS comments;")
+    cursor.execute("DROP TABLE IF EXISTS posts;")
+    cursor.execute("DROP TABLE IF EXISTS users;")
 
-        cursor.execute("CREATE TABLE users(id INT NOT NULL PRIMARY KEY, user_name VARCHAR(16) NOT NULL UNIQUE, public_key INT NOT NULL UNIQUE, info VARCHAR(255));")
-        cursor.execute("CREATE TABLE posts(id INT NOT NULL PRIMARY KEY, user_id INT NOT NULL, post VARCHAR(255) NOT NULL, FOREIGN KEY (user_id) REFERENCES users (id));")
-        cursor.execute("CREATE TABLE comments(id INT NOT NULL PRIMARY KEY, user_id INT NOT NULL, post_id INT NOT NULL, comment VARCHAR(255) NOT NULL, FOREIGN KEY (user_id) REFERENCES users (id), FOREIGN KEY (post_id) REFERENCES posts (id));")
+    cursor.execute("CREATE TABLE users(id INT NOT NULL PRIMARY KEY, user_name VARCHAR(16) NOT NULL UNIQUE, public_key INT NOT NULL UNIQUE, info VARCHAR(255));")
+    cursor.execute("CREATE TABLE posts(id INT NOT NULL PRIMARY KEY, user_id INT NOT NULL, post VARCHAR(255) NOT NULL, FOREIGN KEY (user_id) REFERENCES users (id));")
+    cursor.execute("CREATE TABLE comments(id INT NOT NULL PRIMARY KEY, user_id INT NOT NULL, post_id INT NOT NULL, comment VARCHAR(255) NOT NULL, FOREIGN KEY (user_id) REFERENCES users (id), FOREIGN KEY (post_id) REFERENCES posts (id));")
 
    
-        cursor.execute("DROP TABLE IF EXISTS ips;")
-        cursor.execute("DROP TABLE IF EXISTS connected_ips;")
+    cursor.execute("DROP TABLE IF EXISTS ips;")
+    cursor.execute("DROP TABLE IF EXISTS connected_ips;")
 
-        cursor.execute("CREATE TABLE ips(ip VARCHAR(21) NOT NULL PRIMARY KEY, time_connected INT NOT NULL);")
-        cursor.execute("CREATE TABLE connected_ips(ip VARCHAR(21) NOT NULL PRIMARY KEY, time_connected INT NOT NULL);")
+    cursor.execute("CREATE TABLE ips(ip VARCHAR(21) NOT NULL PRIMARY KEY, time_connected INT NOT NULL);")
+    cursor.execute("CREATE TABLE connected_ips(ip VARCHAR(21) NOT NULL PRIMARY KEY, time_connected INT NOT NULL);")
 
-        connection.commit()
+    connection.commit()
     connection.close()
 
 
