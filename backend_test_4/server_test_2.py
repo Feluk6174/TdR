3da89f26a717f820edca38948c4c09fec0946c97
Feluk6174 <feluk6174@proton.me>
2022-07-07 17:53:26 +0200

    added extra info to debug

--- backend_test_4/server_test_2.py
+++ backend_test_4/server_test_2.py
@@ -36,14 +36,14 @@ def broadcast_ip(ip:str):
     #broadcasts ip to all connections
     global conections
 
-    print(f"[{time.asctime()}]broadcasting:", ip)
+    print(f"({threading.currentThread().name})[{time.asctime()}] broadcasting:", ip)
     for connection in connections:
         connection[2].send(("{"+f'"type": "IP", "ip": "{ip}"'+"}").encode("utf-8"))
 
 def ip_manager(msg_info:str):
     global db, IP
 
-    print(f"[{time.asctime()}]managing:", msg_info)
+    print(f"({threading.currentThread().name})[{time.asctime()}] managing:", msg_info)
     
     ip = msg_info["ip"]
 
@@ -58,13 +58,13 @@ def ip_manager(msg_info:str):
     print(res, int(time.time()) - seconds_to_update, len(res))
 
     if len(res) == 0:
-        print(db.querry("SELECT * FROM ips;"))
+        print("1", db.querry("SELECT * FROM ips;"))
         db.execute(f"INSERT INTO ips(ip, time_connected) VALUES('{ip}', {time.time()});")
         broadcast_ip(ip)
         return
 
     elif res[0][1] <= int(time.time()) - seconds_to_update:
-        print(db.querry("SELECT * FROM ips;"))
+        print("2", db.querry("SELECT * FROM ips;"))
         db.execute(f"DELETE FROM ips WHERE ip = '{ip}';")
         db.execute(f"INSERT INTO ips(ip, time_connected) VALUES('{ip}', {time.time()});")
         broadcast_ip(ip)
@@ -73,6 +73,8 @@ def ip_manager(msg_info:str):
 
 def check_if_connected(ip:str):
     global connections
+
+    print(f"({threading.currentThread().name})[{time.asctime()}] checking if connected:", ip)
     for connection in connections:
         if connection[0] == ip:
             return True
@@ -80,6 +82,8 @@ def check_if_connected(ip:str):
 
 def mainloop(connection, ip):
     global connections, HOST, PORT
+
+    print(f"({threading.currentThread().name})[{time.asctime()}] mainloop:", ip)
     while True:
         try:
             msg_info = json.loads(connection.recv(1024).decode("utf-8"))
@@ -106,7 +110,7 @@ def connect_to_new_node():
     global connections, IP, server_info, get_n_connected
     while True:
         ip = db.querry("SELECT ip FROM ips ORDER BY RAND() LIMIT 1;")
-        print(f"[{time.asctime()}] tring to connect to {ip}")
+        print(f"({threading.currentThread().name})[{time.asctime()}] tring to connect to {ip}")
         n_connected = len(connections)
         n_nodes = len(db.querry("SELECT * FROM ips;"))
         n_suposed_connections = get_n_connected(n_nodes)
@@ -131,6 +135,7 @@ def connect_to_new_node():
 
 def manage_new_node(connection, address, conn_info):
     global connections, get_n_connected, db
+    print(f"({threading.currentThread().name})[{time.asctime()}] managing new node:", ip)
     n_connected = len(connections)
     n_nodes = len(db.querry("SELECT * FROM ips;"))
     n_suposed_connections = get_n_connected(n_nodes)
@@ -144,6 +149,7 @@ def manage_new_node(connection, address, conn_info):
 
 def ip_share_loop():
     global HOST, PORT, IP, connections, db
+    print(f"({threading.currentThread().name})[{time.asctime()}] ip sharing loop:", ip)
     while True:
         print(f"[{time.asctime()}]")
         print("num connecions: ", len(connections))
