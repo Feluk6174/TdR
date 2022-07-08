9ff8bee296051b4763771f9d4efe6e9a9c20569e
Feluk6174 <feluk6174@proton.me>
2022-07-09 00:44:07 +0200

    added info to debug

--- backend_test_5/server.py
+++ backend_test_5/server.py
@@ -64,7 +64,8 @@ def get_posts(msg_info, connection):
 
     connection.send(str(len(posts)).encode("utf-8"))
 
-    for post in posts:
+    for i, post in enumerate(posts):
+        print(i)
         msg = "{"+f'"id": "{post[0]}", "user_id": "{post[1]}", "content": "{post[2]}", "time_posted": {post[3]}'+"}"
         msg.send(msg.encode("utf-8"))
         if not connection.recv(1024).decode("utf-8") == "OK":
