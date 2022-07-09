021a7fb939459ccc14592567c31905a4c89cec11
Feluk6174 <feluk6174@proton.me>
2022-07-09 19:11:01 +0200

    added suport for flags

--- backend_test_5/client_test_1.py
+++ backend_test_5/client_test_1.py
@@ -5,10 +5,10 @@ import api
 
 
 api.register_user("Feluk6174", 6174, "caracaracara", "your mom is a pinnapple")
-api.post("Hello world!", "1", "Feluk6174")
-api.post("Hey aixo ja funciona, sembla", "2", "Feluk6174")
-api.post("I just wanted to save the world", "3", "Feluk6174")
-api.post("ja no se que dir", "4", "Feluk6174")
+api.post("Hello world!", "1", "Feluk6174", "010101")
+api.post("Hey aixo ja funciona, sembla", "2", "Feluk6174", "010101")
+api.post("I just wanted to save the world", "3", "Feluk6174", "010101")
+api.post("ja no se que dir", "4", "Feluk6174", "010101")
 print(api.get_posts("Feluk6174"))
 
 #while True:
--- gui_1/api.py
+++ gui_1/api.py
@@ -12,7 +12,6 @@ time.sleep(1)
 
 def register_user(user_name, public_key, profile_picture, info):
     global connection
-    print(1)
     msg = "{"+f'"type": "REGISTER", "user_name": "{user_name}", "public_key": {public_key}, "profile_picture": "{profile_picture}", "info": "{info}"'+"}"
     connection.send(msg.encode("utf-8"))
     response = connection.recv(1024).decode("utf-8")
@@ -23,7 +22,6 @@ def register_user(user_name, public_key, profile_picture, info):
 
 def post(content, post_id, user_name, flags):
     global connection
-    print(2)
     msg = "{"+f'"type": "POST", "post_id": "{post_id}", "user_name": "{user_name}", "content": "{content}, "flags": "{flags}"'+"}"
     connection.send(msg.encode("utf-8"))
     response = connection.recv(1024).decode("utf-8")
@@ -34,13 +32,11 @@ def post(content, post_id, user_name, flags):
 
 def get_posts(user_name):
     global connection
-    print(3)
     posts = []
     msg = "{"+f'"type": "GET POSTS", "user_name": "{user_name}"'+"}"
     connection.send(msg.encode("utf-8"))
     num = int(connection.recv(1024).decode("utf-8"))
     connection.send("OK".encode("utf-8"))
-    print(num)
     for _ in range(num):
         posts.append(json.loads(connection.recv(1024).decode("utf-8")))
         connection.send("OK".encode("utf-8"))
