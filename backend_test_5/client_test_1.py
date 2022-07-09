3d173acc24f7b574a577f2885dafb896bc103bd0
Feluk6174 <feluk6174@proton.me>
2022-07-09 18:24:37 +0200

    fixed small opsie dopsie

--- backend_test_5/client_test_1.py
+++ backend_test_5/client_test_1.py
@@ -4,12 +4,15 @@ import time
 import api
 
 api.register_user("Feluk6174", 6174, "caracaracara", "your mom is a pinnapple")
-
+time.sleep(1)
 api.post("Hello world!", "1", "Feluk6174")
+time.sleep(1)
 api.post("Hey aixo ja funciona, sembla", "2", "Feluk6174")
+time.sleep(1)
 api.post("I just wanted to save the world", "3", "Feluk6174")
+time.sleep(1)
 api.post("ja no se que dir", "4", "Feluk6174")
-
+time.sleep(1)
 print(api.get_posts("Feluk6174"))
 
 #while True:
