15c4a491a59b3da5812732515e0d5aad84b52aa2
Feluk6174 <feluk6174@proton.me>
2022-07-14 01:02:12 +0200

    started to work on user authentification

--- backend_dev/auth.py
+++ backend_dev/auth.py
@@ -3,25 +3,61 @@ from Crypto.Random import get_random_bytes
 import base64
 from Crypto.Signature import pss
 from Crypto.Hash import SHA256
+import zlib, sys
+
+from yaml import load
 
 def gen_key(passphrase):
-    secret_code = "Unguessable"
     key = RSA.generate(2048)
-    encrypted_key = key.export_key(passphrase=secret_code, pkcs=8,
+    encrypted_key = key.export_key(passphrase=passphrase, pkcs=8,
                                   protection="scryptAndAES128-CBC")
 
-    file_out = open("rsa_key.bin", "wb")
-    file_out.write(encrypted_key)
-    file_out.close()
+    with open("rsa_key.bin", "wb") as f:
+        f.write(encrypted_key)
 
     print(key.publickey().export_key())
+
+def get_keys(passphrase):
+    with open("rsa_key.bin", "rb") as f:
+        encoded_key = f.read()
+        key = RSA.import_key(encoded_key, passphrase=passphrase)
+        pub_key = key.public_key()
+        return key, pub_key
 
 def gen_hash(*args):
     message = ""
     for arg in args:
-        message += arg
+        message += str(arg)
     print(message)
     return SHA256.new(message.encode("utf-8"))
+
+def sanitize_key(key:str):
+    clean_str = ""
+    
+    for i, line in enumerate(key.split("\n")):
+        #print(i)
+        if not i == 0 and not i == len(key.split("\n"))-1:
+            clean_str += line
+
+    return clean_str
+
+
+def reconstruct_key(sanitized_key, key_type="priv"):
+    key = ""
+    i = 0
+    if key_type == "priv":
+        key += "-----BEGIN ENCRYPTED PRIVATE KEY-----\n"
+    num = int(len(sanitized_key)/64)
+    for i in range(num):
+        key += sanitized_key[i*64:i*64+64]+"\n"
+        #print(key)
+    key += sanitized_key[(i+1)*64::]+"\n"
+    
+    if key_type == "priv":
+        key += "-----END ENCRYPTED PRIVATE KEY-----"
+    
+    return key
+
 
 def sign(key, *args):
     h = gen_hash(*args)
@@ -42,12 +78,9 @@ def verify(pub_key, signature, *args):
         return False
 
 if __name__ == "__main__":
-    key = RSA.import_key(open('rsa_key.bin').read(), passphrase="Unguessable")
+    gen_key("heyy")
+    rkey, pub = get_keys("heyy")
+    
+    key = pub.export_key()
 
-    data = "Hola em dic Felix!"
-
-    signature = sign(key, data)
-
-    print(verify(key.public_key(), signature, data))
-
-
+    print(verify(RSA.import_key(key), "skdfjhsd", "skdfjhsd"))
