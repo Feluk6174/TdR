8a85156c8863ac6e2b64f1785a96880f6174d88b
Feluk6174 <feluk6174@proton.me>
2022-07-14 02:19:04 +0200

    added info to debug

--- backend_dev/client_test_1.py
+++ backend_dev/client_test_1.py
@@ -6,6 +6,8 @@ conn = api.Connection()
 auth.gen_key("Hola")
 priv_key, pub_key = auth.get_keys("Hola")
 
+print(pub_key)
+
 with open("rsa_key.bin", "r") as f:
     keys_file = f.read()
 
@@ -16,6 +18,9 @@ conn.post("I just wanted to save the world", "3", "Feluk6174", "0101010101")
 conn.post("ja no se que dir", "4", "Feluk6174", "0101010101")
 print(conn.get_posts("Feluk6174"))
 print(conn.get_user("Feluk6174"))
+
+#a5ea7fc9fb08c80525618acb7d6b5124a3c7f753d8dbbc12d6e669df777ba091
+#a5ea7fc9fb08c80525618acb7d6b5124a3c7f753d8dbbc12d6e669df777ba091
 
 #while True:
 #    command = input("Command: ")
