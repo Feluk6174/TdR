4413843f5a0f69dec17c1a3e69f812e1d43d0a56
Feluk6174 <feluk6174@proton.me>
2022-07-14 13:21:10 +0200

    added info to debug

--- backend_dev/server.py
+++ backend_dev/server.py
@@ -56,6 +56,7 @@ def new_post(msg_info, connection, ip=None):
     pub_key = RSA.import_key(auth.reconstruct_key(pub_key[0][0], key_type="pub"))
     
     print(pub_key.export_key().decode("utf-8"))
+    print(pub_key.export_key())
     print(SHA256.new(pub_key.export_key()).hexdigest())
 
     #signature = auth.sign(priv_key, content, post_id, user_name, flags, time_posted)
