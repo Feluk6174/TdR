cc8464c106c91eaf5616668aaa041117bd85502d
Feluk6174 <feluk6174@proton.me>
2022-07-21 13:29:59 +0200

    redduced db key lengs so that it fits the 3072 bytes limit

--- backend_dev/database.py
+++ backend_dev/database.py
@@ -98,9 +98,9 @@ class Database():
         cursor.execute("DROP TABLE IF EXISTS posts;")
         cursor.execute("DROP TABLE IF EXISTS users;")
 
-        cursor.execute("CREATE TABLE users(user_name VARCHAR(16) COLLATE ascii_general_ci NOT NULL UNIQUE PRIMARY KEY, public_key VARCHAR(392) COLLATE ascii_general_ci NOT NULL UNIQUE, key_file VARCHAR(1764) COLLATE ascii_general_ci NOT NULL UNIQUE, time_created INT NOT NULL, profile_picture COLLATE ascii_general_ci VARCHAR(64) NOT NULL, info VARCHAR(200));")
-        cursor.execute("CREATE TABLE posts(id VARCHAR(23) NOT NULL PRIMARY KEY, user_id VARCHAR(16) NOT NULL, post VARCHAR(255) NOT NULL, flags VARCHAR(10) NOT NULL, time_posted INT NOT NULL, signature VARCHAR(344), FOREIGN KEY (user_id) REFERENCES users (user_name));")
-        cursor.execute("CREATE TABLE comments(id INT NOT NULL PRIMARY KEY, user_id VARCHAR(16) NOT NULL, post_id VARCHAR(23) NOT NULL, comment VARCHAR(255) NOT NULL, FOREIGN KEY (user_id) REFERENCES users (user_name), signature VARCHAR(344), FOREIGN KEY (post_id) REFERENCES posts (id));")
+        cursor.execute("CREATE TABLE users(user_name VARCHAR(16) COLLATE ascii_general_ci NOT NULL UNIQUE PRIMARY KEY, public_key VARCHAR(392) COLLATE ascii_general_ci NOT NULL UNIQUE, key_file VARCHAR(1764) COLLATE ascii_general_ci NOT NULL UNIQUE, time_created INT NOT NULL, profile_picture VARCHAR(64) COLLATE ascii_general_ci NOT NULL, info VARCHAR(200));")
+        cursor.execute("CREATE TABLE posts(id VARCHAR(23) NOT NULL PRIMARY KEY, user_id VARCHAR(16) COLLATE ascii_general_ci NOT NULL, post VARCHAR(255) NOT NULL, flags VARCHAR(10) NOT NULL, time_posted INT NOT NULL, signature VARCHAR(344), FOREIGN KEY (user_id) REFERENCES users (user_name));")
+        cursor.execute("CREATE TABLE comments(id INT NOT NULL PRIMARY KEY, user_id VARCHAR(16) COLLATE ascii_general_ci NOT NULL, post_id VARCHAR(23) NOT NULL, comment VARCHAR(255) NOT NULL, FOREIGN KEY (user_id) REFERENCES users (user_name), signature VARCHAR(344), FOREIGN KEY (post_id) REFERENCES posts (id));")
 
     
         cursor.execute("DROP TABLE IF EXISTS ips;")
